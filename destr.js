3761989188c70e4ef1a5e565206d9439f0848641
Marcelo Bettini <marcelobettini@yahoo.com.ar>
2021-10-01 12:43:44 -0300

    migrated

---
+++ destr.js
@@ -0,0 +1,11 @@
+const horse = {
+  nome: "A horse with no name",
+  age: 6,
+  size: "Large",
+  skills: ["racing", "jousting"],
+};
+
+console.log(horse);
+const { nome, age, size, skills } = horse;
+console.log(nome);
+console.log(horse.nome);
---
+++ routes/about.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+router.get("/", function (req, res, next) {
+  res.render("about");
+});
+module.exports = router;
